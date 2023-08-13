f5d23beb26be373f9c3a8df7658b4b6668c99ceb
Benjamin Segall <bentekkie@gmail.com>
2023-08-13 02:52:41 +0000

    Remove unused yaml config method

--- custom_components/generac/__init__.py
+++ custom_components/generac/__init__.py
@@ -9,7 +9,6 @@ import logging
 from datetime import timedelta
 
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import Config
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
@@ -25,11 +24,6 @@ from .coordinator import GeneracDataUpdateCoordinator
 SCAN_INTERVAL = timedelta(seconds=30)
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
-
-
-async def async_setup(hass: HomeAssistant, config: Config):
-    """Set up this integration using YAML is not supported."""
-    return True
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
